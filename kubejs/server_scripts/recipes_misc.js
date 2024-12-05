24e644347bee8dcba33ed8525e8a4e8e21e59a3a
licorice45 <90569900+licorice45@users.noreply.github.com>
2024-12-05 23:26:41 +0100

    Edited output for rope ladder recipe

--- kubejs/server_scripts/recipes_misc.js
+++ kubejs/server_scripts/recipes_misc.js
@@ -1,6 +1,6 @@
 ServerEvents.recipes(event => {
 	event.shaped(
-		Item.of('spelunkery:rope_ladder'),
+		Item.of('spelunkery:rope_ladder', 8),
 		['R R', 'SSS', 'R R'],
 		{R: 'farmersdelight:rope', S: 'minecraft:stick'}
 	)
